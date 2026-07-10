5534437dee23e2e815f28f165f291e1d82b9125b
anhsakura0531 <anhsakura0531@gmail.com>
2026-07-11 01:20:04 +0900

    Add LINE reply manual site
    Static HTML guide organized into basic info, accounts/systems, emoji
    meanings, main reply flows, retention scripts, coach directory, and
    misc notes, converted from the team's Notion page.

---
+++ coaches.js
@@ -0,0 +1,15 @@
+// コーチが増えたら、この配列に1行追加するだけでOKです。
+// meetUrl / schedulingUrl が無い場合は空文字 '' のままにしてください。
+const COACHES = [
+  { name: '李如來',     tag: 'C1',  meetUrl: 'http://meet.google.com/xeq-jopp-ziv', recordingOk: true,  schedulingUrl: 'https://timerex.net/s/yulae.lee_a805/3993768e' },
+  { name: '井岡澄萌美', tag: 'C2',  meetUrl: 'http://meet.google.com/uax-rgrg-myk', recordingOk: true,  schedulingUrl: '' },
+  { name: '清水鈴',     tag: 'C3',  meetUrl: 'http://meet.google.com/aun-ddvo-zob', recordingOk: true,  schedulingUrl: 'https://timerex.net/s/coaching_1592_d1f5/25dc3dcc' },
+  { name: '福田大輔',   tag: 'C4',  meetUrl: 'http://meet.google.com/afx-qytk-axr', recordingOk: true,  schedulingUrl: 'https://timerex.net/s/daisuke.fukuda.blued_5a73/d117d16e' },
+  { name: '合田彩歩',   tag: 'C5',  meetUrl: 'http://meet.google.com/pmr-jgcy-tbr', recordingOk: true,  schedulingUrl: 'https://timerex.net/s/saho.goda.blued_4024/2d048eb7' },
+  { name: '益子明子',   tag: 'C6',  meetUrl: 'http://meet.google.com/aey-paat-cbx', recordingOk: true,  schedulingUrl: 'https://timerex.net/s/akane.mashiko.blued_6cce/54d11ee1' },
+  { name: '早川瑠泉',   tag: 'C7',  meetUrl: 'http://meet.google.com/hcg-mxca-iaj', recordingOk: true,  schedulingUrl: 'https://timerex.net/s/rumi.hayakawa.blued_bef1/70759def' },
+  { name: '富永咲',     tag: 'C9',  meetUrl: 'http://meet.google.com/vft-mmxr-qep', recordingOk: true,  schedulingUrl: 'https://timerex.net/s/saki.tominaga.blued_8ab2/647add45' },
+  { name: 'グエン',     tag: 'C10', meetUrl: 'http://meet.google.com/hgw-iqck-ztf', recordingOk: true,  schedulingUrl: '' },
+  { name: '玉手',       tag: 'C12', meetUrl: 'http://meet.google.com/wkr-tntt-yio', recordingOk: true,  schedulingUrl: '' },
+  { name: '押方',       tag: 'C13', meetUrl: 'http://meet.google.com/oup-exha-ogs', recordingOk: false, schedulingUrl: '' },
+];
